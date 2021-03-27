a7f872d654e50bb88772db67b615358f415ca227
Admin <mynguyen8333@gmail.com>
2021-03-27 09:55:15 +0700

    lan1

---
+++ Bai2.4/app/src/main/java/com/example/bai24/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bai24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
